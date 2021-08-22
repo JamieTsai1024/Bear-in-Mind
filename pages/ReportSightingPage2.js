0e297ac016e37a3e06438187564d676336b38112
Jamie1024 <jamie.tsai88@gmail.com>
2021-08-22 07:23:16 -0400

    page 2 radio buttons

--- pages/ReportSightingPage2.js
+++ pages/ReportSightingPage2.js
@@ -1,12 +1,13 @@
-import React from 'react';
-import {Text, View, Button, Image, TouchableHighlight} from 'react-native';
+import React, {useState} from 'react';
+import {Text, View, Image, TouchableHighlight} from 'react-native';
 // import {RadioButton} from 'react-native-paper';
 import {styles} from "../assets/style.js";
 // import {useFormik} from 'formik';
+import RadioButton from '../components/RadioButton';
 
 // var danger = "None";
 
-const danger = [
+const dangers = [
   {
     key: "Minor",
     text: "Minor",
@@ -23,7 +24,33 @@ const danger = [
 
 export default function ReportSightingPage2({navigation}) {
   
+  const [selectedOption, setSelectedOption] = useState(null);
+  const [warningText, setWarningText] = useState("");
+
   global.danger = "";
+
+  const onSelect = (item) => {
+    if (selectedOption && selectedOption.key === item.key) {
+      // Set selectedOption to null when "unselecting" the option
+      setSelectedOption(null);
+    } else {
+      // In all other cases, selectedOption can be set to the item
+      setSelectedOption(item);
+    }
+  };
+
+  const moveOn = (item) => {
+    if (selectedOption == null) {
+      showTitle();
+    } else {
+      global.danger = selectedOption.key; 
+      navigation.navigate('Report Sighting 3');
+    }
+  };
+
+  function showTitle() {
+    setWarningText("Please choose an option before continuing!");
+  };
 
   return (
     <View style={styles.containerDark}>
@@ -31,47 +58,20 @@ export default function ReportSightingPage2({navigation}) {
       <Text style={styles.titleLight}>Report a Sighting</Text>
       <Text style={styles.subtitleLight}>How dangerous was the animal?</Text>
 
-      <View style={[styles.horizontalFlex, styles.addMargin]}>
-
-        {/* Minor */}
-        <TouchableHighlight style={[styles.buttonMint, styles.buttonShort]} onPress={() => {
-          global.danger = "Minor";
-          alert("hey " + global.animal);
-          navigation.navigate('Report Sighting 3');
-        }}>
-          <Text style={styles.textDark}>Minor</Text>
-        </TouchableHighlight>
-
-        {/* Moderate */}
-        <TouchableHighlight style={[styles.buttonMint, styles.buttonShort]} onPress={() => {
-          global.danger = "Moderate";
-          navigation.navigate('Report Sighting 3');
-        }}>
-          <Text style={styles.textDark}>Moderate</Text>
-        </TouchableHighlight>
-
-        {/* High */}
-        <TouchableHighlight style={[styles.buttonMint, styles.buttonShort]} onPress={() => {
-          global.danger = "High";
-          navigation.navigate('Report Sighting 3');
-        }}>
-          <Text style={styles.textDark}>High</Text>
-        </TouchableHighlight>
-
-      </View>
-{/* 
-      <TouchableOpacity style={styles.circle} onPress={() => this.setState({ value: key })}>
-        { value === item.key && (<View style={styles.checkedCircle} />) } 
-      </TouchableOpacity> */}
+      <RadioButton selectedOption={selectedOption} onSelect={onSelect} options={dangers}/>
       
       <View style={styles.horizontalFlex}>
         <TouchableHighlight style={[styles.buttonGrey, styles.buttonShort, styles.extraMarginHorizontal]} onPress={() => navigation.navigate('Report Sighting 1')}>
           <Text style={styles.textDark}>{'<'} PREV</Text>
         </TouchableHighlight>
-        <TouchableHighlight style={[styles.buttonMint, styles.buttonShort, styles.extraMarginHorizontal, styles.extraMarginBottom]} onPress={() => navigation.navigate('Report Sighting 3')}>
+        <TouchableHighlight style={[styles.buttonMint, styles.buttonShort, styles.extraMarginHorizontal, styles.moreMarginBottom]} onPress={() => moveOn()}>
           <Text style={styles.textDark}>NEXT {'>'}</Text>
         </TouchableHighlight>
       </View>
+
+      <TouchableHighlight style={[styles.extraMarginHorizontal, styles.extraMarginBottom]}>
+        <Text style={styles.textWarn}>{warningText}</Text>
+      </TouchableHighlight>
 
       <Image source={require('../assets/whiteBirds.png')} style={styles.footerImageBirds}/>
 
